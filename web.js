977608c30eb5be2878bbdc2fe822cd18c9530a42
Masato Ikeda <masato.ikeda@gmail.com>
2011-10-12 09:27:56 +0900

    add my method

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res) {
-    res.send('Hello, Hello, My World!!');
-  });
+  res.send('Hello, Hello, My World!!');
+});
+
+app.get('/:id', function(req, res) {
+  res.send('id = ' + req.params.id);
+}); 
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
-  });
+  console.log("Listening on " + port);
+});
 
